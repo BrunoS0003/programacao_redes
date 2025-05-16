a829d2fc2f2e8a5cbc8912e02cd23c6deb43bf71
BrunoS0003 <brunoseabrapopoca@gmail.com>
2025-05-16 20:10:10 -0300

    resolução da questão 7

---
+++ q7.py
@@ -0,0 +1,9 @@
+numeroSwitchs = int(input("digite o número de switchs da rede: "))
+
+switchs = []
+for i in range(numeroSwitches):
+    portas = int(input("Digite o número de portas do switch " + str(i+1) + "? "))
+    switchs.append(portas)
+
+for switch in switchs:
+    print("Número de portas: " + str(switch))    
