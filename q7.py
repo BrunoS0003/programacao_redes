f41a58e297f4b8dc60530ad83274d261f19e1a99
BrunoS0003 <brunoseabrapopoca@gmail.com>
2025-05-16 21:43:03 -0300

    resolução da questão 8

--- q7.py
+++ q7.py
@@ -1,7 +1,7 @@
 numeroSwitchs = int(input("digite o número de switchs da rede: "))
 
 switchs = []
-for i in range(numeroSwitches):
+for i in range(numeroSwitchs):
     portas = int(input("Digite o número de portas do switch " + str(i+1) + "? "))
     switchs.append(portas)
 
---
+++ q9.py
@@ -0,0 +1,5 @@
+mac = input("Digite um endereço MAC de um dispositivo: ")
+if mac.startswith("00:1A:2B"):
+    print("pertence ao fabricante")
+else:
+    print("Não pertence")
